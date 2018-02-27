aad596df6fe446755e16414997839f992a0f0cc5
Dongwoo Kim <kimdwkimdw@gmail.com>
2018-02-27 18:51:38 +0900

    modified layers_depth

--- network_fusionnet.py
+++ network_fusionnet.py
@@ -30,7 +30,7 @@ class NetworkFusionNet(NetworkBasic):
             'is_training': self.is_training,
             'center': True,
             'scale': False,
-            'decay': 0.9,
+            'decay': 0.97,
             'epsilon': 0.001,
             'fused': True,
             # 'zero_debias_moving_mean': True
@@ -41,24 +41,29 @@ class NetworkFusionNet(NetworkBasic):
             'weights_initializer': tf.truncated_normal_initializer(mean=0.0, stddev=0.001),
             'normalizer_fn': slim.batch_norm,
             'normalizer_params': batch_norm_params,
-            'activation_fn': tf.nn.relu
+            'activation_fn': tf.nn.elu
         }
 
         net = self.input_batch
         features = []
         with slim.arg_scope([slim.convolution, slim.conv2d_transpose], **conv_args):
-            for i in range(3):
+            layers_depth = 3
+            for i in range(layers_depth):
                 net = __class__.conv_res_conv(net, int(64 * (2 ** i)), scope='down_conv_res_conv_%d' % (i + 1))
                 features.append(net)
                 net = slim.max_pool2d(net, [3, 3], 2, padding='SAME', scope='pool%d' % (i + 1))
-            net = __class__.conv_res_conv(net, 512, scope='middle_conv_1')
 
-            for i in range(3):
-                net = slim.conv2d_transpose(net, 256 // (2 ** i), [3, 3], 2, scope='up_trans_conv_%d' % (i + 1))
+            mid_conv_size = 64 * (2**layers_depth)
+            net = __class__.conv_res_conv(net, mid_conv_size, scope='middle_conv_1')
+
+            down_conv_size = mid_conv_size // 2
+            for i in range(layers_depth):
+                current_down_conv_size = down_conv_size // (2 ** i)
+                net = slim.conv2d_transpose(net, current_down_conv_size, [3, 3], 2, scope='up_trans_conv_%d' % (i + 1))
                 down_feat = features.pop()  # upsample with origin version
                 # net = tf.concat([down_feat, net], axis=-1)
                 net = down_feat + net
-                net = __class__.conv_res_conv(net, 256 // (2 ** i), scope='up_conv_%d' % (i + 1))
+                net = __class__.conv_res_conv(net, current_down_conv_size, scope='up_conv_%d' % (i + 1))
 
         net = slim.convolution(net, 1, [3, 3], 1, scope='final_conv',
                                padding='SAME',
