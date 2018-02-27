e2ac4d18d83f884835829657fc103f4513583d24
Dongwoo Kim <kimdwkimdw@gmail.com>
2018-02-27 17:35:24 +0900

    FusionNet starter code

---
+++ network_fusionnet.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+from tensorflow.contrib import slim
+from network_basic import NetworkBasic
+
+
+class NetworkFusionNet(NetworkBasic):
+    def __init__(self, batchsize):
+        super(NetworkFusionNet, self).__init__(batchsize=batchsize)
+
+    # ResBlock
+    @staticmethod
+    def res_block(net, nb_filter, scope):
+        residual = net
+        net = slim.convolution(net, nb_filter, [3, 3], 1, scope='%s_res_1' % scope)
+        net = slim.convolution(net, nb_filter, [3, 3], 1, scope='%s_res_2' % scope)
+        net = slim.convolution(net, nb_filter, [3, 3], 1, scope='%s_res_3' % scope)
+        return net + residual
+
+    # ConvResConv
+    @staticmethod
+    def conv_res_conv(net, nb_filter, scope):
+        net = slim.convolution(net, nb_filter, [3, 3], 1, scope='%s_1' % scope)
+        net = __class__.res_block(net, nb_filter, scope)
+        net = slim.convolution(net, nb_filter, [3, 3], 1, scope='%s_2' % scope)
+        return net
+
+    def build(self):
+        # https://github.com/tensorflow/tensorflow/blob/master/tensorflow/contrib/layers/python/layers/layers.py#L429
+
+        net = self.input_batch
+        features = []
+        for i in range(4):
+            net = __class__.conv_res_conv(net, int(64 * (2 ** i)), scope='down_conv_res_conv_%d' % (i + 1))
+            features.append(net)
+            net = slim.max_pool2d(net, [3, 3], 2, padding='SAME', scope='pool%d' % (i + 1))
+        net = __class__.conv_res_conv(net, 512, scope='middle_conv_1')
+
+        for i in range(4):
+            net = slim.conv2d_transpose(net, 256 // (2 ** i), [3, 3], 2, scope='up_trans_conv_%d' % (i + 1))
+            down_feat = features.pop()  # upsample with origin version
+            net = tf.concat([down_feat, net], axis=-1)
+            net = __class__.conv_res_conv(net, 256 // (2 ** i), scope='up_conv_%d' % (i + 1))
+
+        net = slim.convolution(net, 1, [3, 3], 1, scope='final_conv',
+                               padding='SAME',
+                               weights_initializer=tf.truncated_normal_initializer(mean=0.0, stddev=0.01))
+
+        self.logit = net
+        self.output = tf.nn.sigmoid(net, 'visualization')
+        self.loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.mask_batch, logits=self.logit))
+        return net
